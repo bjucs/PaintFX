95b86f7a42167bb78f792066b0488b53bed4f812
Brian Ju <91577965+bjucs@users.noreply.github.com>
2022-08-15 09:29:45 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+
+public class Main extends javafx.application.Application {
+
+    public static void Main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = new File("src/main/java/org/dawiddc/paint/view/MainPane.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+
+        primaryStage.setTitle("PaintFX");
+        primaryStage.setScene(new Scene(root, 1280.0, 960.0));
+        primaryStage.getIcons().add(new Image("assets/logo.png"));
+        primaryStage.show();
+    }
+}
