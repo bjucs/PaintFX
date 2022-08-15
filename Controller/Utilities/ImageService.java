b7235952ce0aacc034dd74356416e548dc987109
Brian Ju <91577965+bjucs@users.noreply.github.com>
2022-08-15 09:35:34 -0400

    Create ImageService.java

---
+++ Controller/Utilities/ImageService.java
@@ -0,0 +1,24 @@
+import javafx.scene.image.Image;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import java.io.ByteArrayInputStream;
+
+public class ImageService {
+
+    public ImageService() {
+        nu.pattern.OpenCV.loadShared();
+    }
+
+    public Mat loadMatrix(String filePath) {
+        return Imgcodecs.imread(filePath);
+    }
+
+    public Image convertToImage(Mat matrix) {
+
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".png", matrix, buffer);
+        return new Image(new ByteArrayInputStream(buffer.toArray()));
+    }
+}
