d9eb0d5a75f98c61f0b15083c80789c6bc63a393
Brian Ju <91577965+bjucs@users.noreply.github.com>
2022-08-15 09:37:40 -0400

    Create Plugin.java

---
+++ Model/Plugin/Plugin.java
@@ -0,0 +1,21 @@
+import org.opencv.core.Mat;
+
+public abstract class Plugin {
+
+    private String name;
+
+    public Plugin(String name) {
+        this.name = name;
+    }
+
+    public abstract Mat run(Mat image);
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
